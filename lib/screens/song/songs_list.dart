bbcabc27c2240faa5cf28ef70c8c08bb22e2d5ab
bezamo <bezademamu@gmail.com>
2021-02-15 03:49:56 -0800

    feat: add song song list

---
+++ lib/screens/song/songs_list.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:waweezer_mobile/bloc/song/song_bloc.dart';
+import 'package:waweezer_mobile/bloc/song/song_state.dart';
+import 'package:waweezer_mobile/widgets/song/song_list_item.dart';
+
+class SongsList extends StatefulWidget {
+  @override
+  _SongsListState createState() => _SongsListState();
+}
+
+class _SongsListState extends State<SongsList> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SongBloc, SongState>(
+      builder: (context, state) => Container(
+        child: state is MultipleSongsOperationSuccess
+            ? ListView.builder(
+                itemCount: state.songs.length,
+                itemBuilder: (context, index) =>
+                    SongListItem(song: state.songs[index]),
+              )
+            : Container(
+                child: Center(
+                  child: (state is SongOperationInProgress)
+                      ? Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            CircularProgressIndicator(),
+                            Text("Loading songs"),
+                          ],
+                        )
+                      : state is SongOperationFailed
+                          ? Text(state.error)
+                          : Text(state.toString()),
+                ),
+              ),
+      ),
+    );
+  }
+}
