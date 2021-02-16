14cfda566707cac6c443bd934e66142d774b8c41
firi193 <56671623+firi193@users.noreply.github.com>
2021-02-16 13:11:20 +0300

    feature: add saved playlist

---
+++ lib/screens/saved_playlists/saved_playlists_list.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:waweezer_mobile/bloc/saved_playlist/saved_playlist_bloc.dart';
+import 'package:waweezer_mobile/bloc/saved_playlist/saved_playlist_state.dart';
+import 'package:waweezer_mobile/widgets/saved_playlists/saved_playlist_list_item.dart';
+
+class SavedPlaylistsList extends StatefulWidget {
+  @override
+  _SavedPlaylistsListState createState() => _SavedPlaylistsListState();
+}
+
+class _SavedPlaylistsListState extends State<SavedPlaylistsList> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SavedPlaylistBloc, SavedPlaylistState>(
+      builder: (context, state) => Container(
+        child: state is SavedPlaylistOperationSuccess
+            ? ListView.builder(
+                itemCount: state.playlists.length,
+                itemBuilder: (context, index) =>
+                    SavedPlaylistListItem(playlist: state.playlists[index]),
+              )
+            : Container(
+                child: Center(
+                  child: (state is SavedPlaylistOperationInProgress)
+                      ? Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            CircularProgressIndicator(),
+                            Text("Loading playlists"),
+                          ],
+                        )
+                      : state is SavedPlaylistOperationFailed
+                          ? Text(state.error)
+                          : Text(state.toString()),
+                ),
+              ),
+      ),
+    );
+  }
+}
