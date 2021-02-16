67e308a32a1248f5845f93782c2c3a51677bd21e
hellyab <yabsra23@gmail.com>
2021-02-16 17:23:13 +0300

    feat: create an empty app chore: clean out the flutter sample chore: setup project structure chore: add flutter_bloc as dependency

---
+++ android/app/src/main/kotlin/com/example/waweezer_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.waweezer_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:waweezer_mobile/screens/home.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
